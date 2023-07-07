d1c332bd60acc60318c51f8794b45cf3ba69f3bf
MeSka338 <halk49114@gmail.com>
2023-07-07 15:47:45 +0300

    change styles

--- components/animation.js
+++ components/animation.js
@@ -1,5 +1,3 @@
-import "../style.css";
-
 import * as THREE from "three";
 import { FontLoader } from "three/examples/jsm/loaders/FontLoader";
 import { TextGeometry } from "three/examples/jsm/geometries/TextGeometry";
